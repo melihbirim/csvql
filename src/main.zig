68050d27971940dbe0fdf9077f07a67f14552a7f
Melih Birim <melihbirim@gmail.com>
2026-08-10 11:45:53 +0300

    chore: bump version to 2.1.1

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,7 @@ const audit = @import("audit.zig");
 const zigtable = @import("zigtable");
 const Allocator = std.mem.Allocator;
 
-const version = "2.1.0";
+const version = "2.1.1";
 
 const help_text =
     \\csvql — the world's fastest CSV query engine
